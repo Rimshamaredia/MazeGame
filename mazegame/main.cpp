d42d6e4d656a60253de4de134c0da2c566721306
Rimsha Maredia <rimshamaredia@main-10-231-234-151.tamulink.tamu.edu>
2018-11-14 12:06:44 -0600

    Initial Commit

---
+++ mazegame/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  mazegame
+//
+//  Created by Rimsha Maredia on 11/14/18.
+//  Copyright © 2018 Rimsha Maredia. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
